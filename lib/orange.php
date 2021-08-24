41bf7ce32e540a69ec740774dee0a691589c3c38
navid59 <torabazary@yahoo.com>
2021-08-24 18:05:34 +0300

    Update - Orange class - but is not tested.

--- lib/orange.php
+++ lib/orange.php
@@ -3,8 +3,35 @@
  * pentru Orange 
  * la orange se taxeaza din prima
  */
-class Orange {
+require_once('lib/notify.php');
 
+class Orange extends SmsOffline {
+	public $reciveMsg; 			 // the message what Cellphone owner sent
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * initiem plata
+	 */
+	public function makeOperation() {
+		$merchant  = new Notify();
+		$merchant->verifyURL = $this->merchantVerifyURL;
+		$verifyArr = array(
+			'sender' 	=> $this->phoneNumber
+		);
+
+		switch($this->reciveMsg) {
+			case $this->uniqueCode:
+				// la orange se taxeaza din prima
+				$this->sendResponse( "Va multumim pentru plata efectuata! Comanda Dvs a confirmat cu success.", 1, 0 );
+			break;
+			default :
+				// in cazul in care codul nu este cel pe care il asteptam
+				$this->sendResponse( "Mesajul nu este corect, va rugam trimiteti la " . $this->mobilpayShortNumber . " un SMS cu codul " . $this->uniqueCode, 0, 0 );
+			break;
+		}
+	}
 }
 
 
